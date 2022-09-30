80714047f12e4f177d531348f298d4dd1aa7743b
SebastienDuhamel <sebastien.duhamel81@gmail.com>
2022-09-30 17:03:05 +0200

    Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <h1 class="card__title">Connexion</h1>
+    <p class="card__subtitle">Tu n'as pas encore de compte ? </p><span></span>
+    <div class="form-row">
+      <input type="text" class="form-row__input" placeholder="Adresse mail"/>
+    </div>
+    <div class="form-row">
+      <input type="text" class="form-row__input" placeholder="Prénom"/>
+      <input type="text" class="form-row__input" placeholder="Nom"/>
+    </div>
+    <div class="form-row">
+      <input type="password" class="form-row__input" placeholder="Mot de Passe"/>
+    </div>
+    <div class="form-row">
+      <button class="button button disabled">
+        Connexion
+      </button>
+    </div>
+  </div>
+</template>
