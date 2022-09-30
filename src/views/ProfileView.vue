6b8648deaeaf131c6e56871f21f46475db0e877c
SebastienDuhamel <sebastien.duhamel81@gmail.com>
2022-09-30 16:54:00 +0200

    Profile.vue

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <h1 class="card__title">Espace Perso</h1>
+    <p class="card__subtitle">Voilà donc qui je suis...</p>
+    <img src="" alt="">
+    <div class="form-row">
+      <button class="button">
+        Déconnexion
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileView'
+}
+</script>
+
+<style scoped>
+
+</style>
